1a8630f01bcc34811cd62d2f2d3c761c40919b77
dsinkerii <arseniy.pupkin@gmail.com>
2024-01-07 03:42:50 +0500

    0.0.2

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ newDiv.innerHTML = `
         <source src=` + videoUrl + ` type="video/mp4">
         Your browser does not support the video tag.
     </video>
-    <video width="160" height="90" muted id="typing-video-left" style="position: absolute;left: 0;filter: opacity(0%);">
+    <video width="160" height="90" muted id="typing-video-left" style="position: absolute;left: 0;top: 0px;filter: opacity(0%);">
         <source src=` + videoUrlTypeLeft + ` type="video/mp4">
         Your browser does not support the video tag.
     </video>
-    <video width="160" height="90" muted id="typing-video-right" style="position: absolute;left: 0;filter: opacity(0%);">
+    <video width="160" height="90" muted id="typing-video-right" style="position: absolute;left: 0;top: 0px;filter: opacity(0%);">
         <source src=` + videoUrlTypeRight + ` type="video/mp4">
         Your browser does not support the video tag.
     </video>
-    <video width="160" height="90" muted id="typing-video-success" style="position: absolute;left: 0;filter: opacity(0%);">
+    <video width="160" height="90" muted id="typing-video-success" style="position: absolute;left: 0;top: 0px;filter: opacity(0%);">
         <source src=` + videoUrlSuccess + ` type="video/mp4">
         Your browser does not support the video tag.
     </video>
@@ -37,19 +37,46 @@ newDiv.innerHTML = `
 <style>
     
 #kiryu {
-    position: fixed;
-    margin: ` + (currentURL.includes("http://127.0.0.1:8888/search") == true ? "200" : "-50") + `px;
-    top: `+ (currentURL.includes("http://127.0.0.1:8888/search") == true ? "-185" : "75") + `px;
+    position: ` + (currentURL == 'https://www.google.com/'|| currentURL == 'https://search.brave.com/' ? "absolute" : "fixed") + `;
+    margin: ` + (currentURL.includes("http://127.0.0.1:8888/search") == true
+    ? "200px" 
+    : currentURL == 'https://www.google.com/' 
+        ? "25%" 
+        : "-50px") + `;
+    top: `+ (currentURL.includes("http://127.0.0.1:8888/search") == true
+    ? "-185px" 
+    : currentURL == 'https://www.google.com/'
+        ? "-100%" 
+        : currentURL == 'https://search.brave.com/' 
+            ? "35%"
+            : "75px") + `;
     background-color: #505052;
+    ` + (currentURL == 'https://search.brave.com/' ? "left: 50%;" : "\n") + `
 }
 </style>
 `;
 
-if(currentURL.includes("www.google.com/search")){
+if(currentURL == 'https://www.google.com/'){
+    var logoElement = document.getElementsByTagName("body")[0].getElementsByTagName("div")[16].getElementsByTagName("div")[0];
+    logoElement.getElementsByTagName("img")[0].style.filter = "opacity(0%)";
+    var Kiryuoff = document.createElement("kiryuOffset");
+    Kiryuoff.appendChild(newDiv);
+    logoElement.appendChild(Kiryuoff);
+}
+else if(currentURL.includes("www.google.com/search")){
     var logoElement = document.getElementById("logo");
     logoElement.appendChild(newDiv);
 }else if(currentURL.includes("http://127.0.0.1:8888/search")){ // in case you use searxng like me
     var logoElement = document.getElementById("search_header");
+    logoElement.appendChild(newDiv);
+}
+else if(currentURL == "https://search.brave.com/"){
+    var logoElement = document.getElementById("logo");
+    logoElement.getElementsByTagName("img")[0].style.filter = "opacity(0%)";
+    logoElement.appendChild(newDiv);
+}
+else if(currentURL.includes("search.brave.com/search")){
+    var logoElement = document.getElementsByClassName("nav-logo btn btn-link svelte-1og62gc")[0];
     logoElement.appendChild(newDiv);
 }
 //key presses
@@ -65,7 +92,27 @@ const delay = (delayInms) => {
 };
 var isPlaying = false;
 
-async function f() {
+var typing = false;
+
+async function Success(event) {
+    if(!isPlaying){
+        isPlaying = true;
+        curVidSuccess.style.filter = "opacity(100%)";
+        curVidTextSuccess.style.filter = "opacity(100%)";
+        curVidSuccess.currentTime = '0';
+        curVidTextSuccess.currentTime = '0';
+        curVidTextSuccess.play();
+        curVidSuccess.play();
+        await delay(2020);
+        curVidSuccess.pause();
+        curVidTextSuccess.pause();
+        curVidTextSuccess.style.filter = "opacity(0%)";
+        curVidSuccess.style.filter = "opacity(0%)";
+        isPlaying = false;
+    }
+}
+
+async function f(event) {
     if(!isPlaying){
         isPlaying = true;
         if(Math.floor(Math.random() * 2) == 0){
@@ -84,34 +131,25 @@ async function f() {
             curVidRight.style.filter = "opacity(0%)";
         }
     isPlaying = false;
+    await delay(750);
+    typing = false;
+    await delay(750);
+    if(!typing){
+        Success(event);
+    }
     }
   }
-async function Success(event) {
-    if(!isPlaying){
-        isPlaying = true;
-        curVidSuccess.style.filter = "opacity(100%)";
-        curVidTextSuccess.style.filter = "opacity(100%)";
-        curVidSuccess.currentTime = '0';
-        curVidTextSuccess.currentTime = '0';
-        curVidTextSuccess.play();
-        curVidSuccess.play();
-        await delay(2020);
-        curVidSuccess.pause();
-        curVidTextSuccess.pause();
-        curVidTextSuccess.style.filter = "opacity(0%)";
-        curVidSuccess.style.filter = "opacity(0%)";
-        isPlaying = false;
-    }
-}
 addEventListener("keydown", (event) => {
     if (event.isComposing || event.which === 229) {
       return;
     }
     if(event.key == "Enter"){
-        event.preventDefault(); 
         Success(event);
-    }else
-        f();
+
+    }else{
+        typing = true;
+        f(event);
+    }
     /*
     curVid.pause();
     curVid.currentTime = '0';
